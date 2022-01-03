4226fcd084da7519b90dd5beeb0d48dd3654db4d
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-01-03 18:35:12 +0500

    Update request.rb

--- lib/util/request.rb
+++ lib/util/request.rb
@@ -1,2 +1,45 @@
 # request.rb
 # > back
+
+require "httparty"
+
+
+class Req
+  attr_accessor :auth
+  attr_accessor :headers
+  attr_accessor :url
+
+  def initialize (auth)
+    @auth = auth
+    @get = HTTParty.get
+    @post = HTTParty.post
+    @patch = HTTParty.patch
+    @delete = HTTParty.delete
+    
+    @headers = {
+      "Authorization" => "Bearer " + @auth
+    }
+    @url = "https://oauth.reddit.com/api/v1/me/"
+  end
+
+
+  def get (url)
+    return @get(@url + url, :headers => @headers)
+  end
+
+  def post (url)
+    return @post(@url + url, :headers => @headers)
+  end
+
+  def patch (url)
+    return @patch(@url + url, :headers => @headers)
+  end
+
+  def delete (url)
+    return @delete(@url + url, :headers => @headers)
+  end
+
+
+
+
+end
