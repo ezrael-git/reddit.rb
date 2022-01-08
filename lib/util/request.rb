a222c68c3353f9d9af047c0fc7b03488b9c4fedd
Azrael <91268618+ezrael-git@users.noreply.github.com>
2022-01-08 20:13:36 +0500

    to JS

--- lib/util/request.rb
+++ lib/util/request.rb
@@ -1,45 +1,86 @@
-# request.rb
-# > back
+// request.js
+// > back 
 
-require "httparty"
+const internal = require("./config.js");
+const axios = require("axios");
+
+// For requesting from the Reddit API
+
+class Request {
+
+  constructor (kwargs) {
+    this.api_key = kwargs["api_key"];
+    this.headers = internal.headers;
+    this.headers["Authorization"] = "Bearer " + this._api_key;
+    this.red_url = internal.url;
+  }
+
+  // generate resource url
+  gen_url (req) {
+    return this.red_url + req
+  }
+
+  // send get request
+  async get (req) {
+    let res = await axios.get(this.red_url + req, {
+      headers: this._headers
+    })
+      .then(function (response) {
+        return response;
+      })
+      .catch(function (error) {
+        return error;
+      });
+
+    return res.data;
+  }
+
+  async delete (req) {
+    let res = await axios.delete(this.red_url + req, {
+      headers: this._headers
+    })
+      .then(function (response) {
+        return response;
+      })
+      .catch(function (error) {
+        return error;
+      });
+
+    return res.data;
+  }
+
+  async post (req) {
+    let res = await axios.post(this.red_url + req, {
+      headers: this._headers
+    })
+      .then(function (response) {
+        return response;
+      })
+      .catch(function (error) {
+        return error;
+      });
+
+    return res.data;
+  }
+
+  async patch (req) {
+    let res = await axios.patch(this.red_url + req, {
+      headers: this._headers
+    })
+      .then(function (response) {
+        return response;
+      })
+      .catch(function (error) {
+        return error;
+      });
+
+    return res.data;
+  }
 
 
-class Request
-  attr_accessor :auth
-  attr_accessor :headers
-  attr_accessor :url
 
-  def initialize (auth)
-    @auth = auth
-    @get = HTTParty.get
-    @post = HTTParty.post
-    @patch = HTTParty.patch
-    @delete = HTTParty.delete
-    
-    @headers = {
-      "Authorization" => "Bearer " + @auth
-    }
-    @url = "https://oauth.reddit.com/api/v1/" # can be changed
-  end
+  
+}
 
 
-  def get (url)
-    return @get(@url + url, :headers => @headers)
-  end
-
-  def post (url)
-    return @post(@url + url, :headers => @headers)
-  end
-
-  def patch (url)
-    return @patch(@url + url, :headers => @headers)
-  end
-
-  def delete (url)
-    return @delete(@url + url, :headers => @headers)
-  end
-
-
-
-
-end
+module.exports = Request;
